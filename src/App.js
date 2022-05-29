f8539e48faa952bfe9800cc4507335b39fc3677b
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-29 17:56:16 -0300

    Implement search users method

--- src/App.js
+++ src/App.js
@@ -5,31 +5,59 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.axiosInstance = axios.create({
+      baseURL: 'https://api.github.com',
+      timeout: 10000,
+    });
+    this.clientId = process.env.REACT_APP_GITHUB_FINDER_CLIENT_ID;
+    this.clientSecret = process.env.REACT_APP_GITHUB_FINDER_CLIENT_SECRET
+    this.authQuery = `client_id=${this.clientId}&client_secret${this.client_secret}`;
+  }
+
   state = {
+    error: false,
+    errorMessage: '',
     loading: false,
     users: [],
   }
 
-  async componentDidMount() {
-    const clientId = process.env.REACT_APP_GITHUB_FINDER_CLIENT_ID;
-    const clientSecret = process.env.REACT_APP_GITHUB_FINDER_CLIENT_SECRET
+  searchUsers = async (userLogin) => {
+    try {
+      this.setState({ loading: true });
+      const res = await this.axiosInstance.get(
+        `search/users?q=${userLogin}&${this.authQuery}`
+      );
+      this.setState({ loading: false, users: res.data.items });
+    } catch(error) {
+      this.setState({
+        users: [],
+        loading: false,
+        error: true,
+        errorMessage: error.message
+      });
+    }
+  }
 
+  async componentDidMount() {
     this.setState({loading: true});
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${clientId}&client_secret=${clientSecret}`
-    );
+    const res = await this.axiosInstance.get(`users?client_id=${this.authQuery}`);
     this.setState({ loading: false, users: res.data });
   }
 
   render () {
-    const { loading, users } = this.state;
+    const { error, errorMessage, loading, users } = this.state;
 
     return (
       <div className="App">
         <Navbar />
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
+          {error && <p className="text-danger text-center">{errorMessage}</p>}
           <Users loading={loading} users={users} />
         </div>
       </div>
