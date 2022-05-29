ddffbffe0253697ef95283b50c55252f4b850b08
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-29 16:01:26 -0300

    Render users list from props

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,32 +1,15 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import UserItem from './UserItem'
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        "login": "mojombo",
-        "id": 1,
-        "avatar_url": "https://avatars.githubusercontent.com/u/1?v=4",
-        "html_url": "https://github.com/mojombo",
-      },
-      {
-        "login": "defunkt",
-        "id": 2,
-        "avatar_url": "https://avatars.githubusercontent.com/u/2?v=4",
-        "html_url": "https://github.com/defunkt",
-      },
-      {
-        "login": "pjhyett",
-        "id": 3,
-        "avatar_url": "https://avatars.githubusercontent.com/u/3?v=4",
-        "html_url": "https://github.com/pjhyett",
-      },
-    ]
+  static propTypes = {
+    users: PropTypes.array.isRequired,
   }
+
   render() {
-    const { users } = this.state;
+    const { users } = this.props;
 
     return (
       <ul className="list" style={usersStyle}>
