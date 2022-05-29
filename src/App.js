0c1beb9dac04ac834836798d4444b021503bc266
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-29 14:32:19 -0300

    Creates Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar';
+
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Hello From Github Finder React App</h1>
+        <Navbar />
       </div>
     );
   }
