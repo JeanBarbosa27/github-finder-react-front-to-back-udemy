b3001aa58c26d54c909ec0aa6c754b1314fcf2e0
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-29 16:40:51 -0300

    Authorizes app to make many requests to github api

--- src/App.js
+++ src/App.js
@@ -13,8 +13,13 @@ class App extends Component {
   }
 
   async componentDidMount() {
+    const clientId = process.env.REACT_APP_GITHUB_FINDER_CLIENT_ID;
+    const clientSecret = process.env.REACT_APP_GITHUB_FINDER_CLIENT_SECRET
+
     this.setState({loading: true});
-    const res = await axios.get('https://api.github.com/users');
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${clientId}&client_secret=${clientSecret}`
+    );
     this.setState({ loading: false, users: res.data });
   }
 
