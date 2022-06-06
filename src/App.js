4245c9b3659be2c4beaae6f45c29d834d1585162
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-06-06 07:07:40 -0300

    Adding a "not found" page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import GithubState from './contexts/github/GithubState';
 import About from './pages/About';
 import Home from './pages/Home';
 import User from './pages/User';
+import NotFound from './pages/NotFound';
 
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
@@ -26,6 +27,7 @@ const App = () => {
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
               <Route exact path="/user/:login" element={<User />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1>Page not found</h1>
+      <p className="lead">The page you're looking for doesn't exist!</p>
+    </Fragment>
+  )
+}
+
+export default NotFound;
